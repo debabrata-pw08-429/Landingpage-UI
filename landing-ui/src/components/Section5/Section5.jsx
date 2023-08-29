bd7392063ae89d09a803d5a009e3d78127597ff7
debabrata-pw08-429 <chintuthecoder@gmail.com>
2023-08-29 20:26:38 +0530

    Errors handled

--- landing-ui/src/components/Section5/Section5.jsx
+++
@@ -1,7 +0,0 @@
-import "./section5.css";
-
-const Section5 = () => {
-  return <div>Section5</div>;
-};
-
-export default Section5;
--- landing-ui/src/pages/LandingPage.jsx
+++ landing-ui/src/pages/LandingPage.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar/Navbar";
 import Section1 from "../components/Section1/Section1";
 import Section2 from "../components/Section2/Section2";
 import Section3 from "../components/Section3/Section3";
+
 import Section6 from "../components/Section6/Section6";
 
 const LandingPage = () => {
@@ -14,6 +15,7 @@ const LandingPage = () => {
       <Section1 />
       <Section2 />
       <Section3 />
+
       <Section6 />
       <Footer />
     </>
